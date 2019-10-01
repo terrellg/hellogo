c1af3303471ab78912605e3249e5acd35f02bef7
Terrell <terrellgreen56@gmail.com>
2019-10-01 11:50:19 -0500

    locale changes

--- main.go
+++ main.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello Go\n")
+	var locale string = "de"
+	var greeting string = "Hello"
+
+	if locale == "en" {
+		greeting = "Hello"
+
+	} else if locale == "es" {
+		greeting = "Hola"
+	} else if locale == "de" {
+		greeting = "Guten Tag"
+	} else {
+		greeting = "Yurrr"
+	}
+
+	fmt.Printf(greeting + " Go\n")
 
 }
